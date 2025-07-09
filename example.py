ecb87a174ab3bdd9f62578379e15d25f20028e8f
endcreeper861 <94234877+endcreeper861@users.noreply.github.com>
2025-07-09 22:42:41 +0800

    将middle修改为center。middle作为垂直居中对齐参数。

--- example.py
+++ example.py
@@ -14,7 +14,7 @@ sign.save("century_avenue.png")
 
 # 生成站台导向标识
 sign = Sign(2048, 128)
-sign.add_element(NumberLine(108, 1), "middle")
+sign.add_element(NumberLine(108, 1), "center")
 sign.add_element(Arrow(108, direction="left"))
 sign.add_element(BilingualText(108, "往富锦路", "To Fujing Road"))
 sign.add_element(BilingualText(108, "往莘庄", "To Xinzhuang"), "right")
@@ -23,12 +23,12 @@ sign.save("platform_sign.png")
 
 # 生成出口标识
 sign = Sign(1024, 256)
-sign.add_element(ExitSign(108, "1,5-22"), "middle")
-sign.add_element(Arrow(108, EXIT_COLOR, direction="up"), "middle")
+sign.add_element(ExitSign(108, "1,5-22"), "center")
+sign.add_element(Arrow(108, EXIT_COLOR, direction="up"), "center")
 sign.save("exit_sign.png")
 
 # 自定义导向标识牌的背景色
 sign = Sign(1024, 256, "#026AA7", padding=64)
-sign.add_element(BilingualText(128, "市域机场线", "Airport Link Line"), "middle")
-sign.add_element(Arrow(128, direction="down"), "middle")
+sign.add_element(BilingualText(128, "市域机场线", "Airport Link Line"), "center")
+sign.add_element(Arrow(128, direction="down"), "center")
 sign.save("airport_link_line.png")
